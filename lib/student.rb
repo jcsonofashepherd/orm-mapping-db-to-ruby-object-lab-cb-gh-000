e68962384e0ec36d415ffdaa658bc217bacd414c
John Casabar <johncasabar@gmail.com>
2018-10-25 02:42:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -91,4 +91,21 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      LIMIT ?
+    SQL
+
+    DB[:conn].execute(sql, num).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.first_student_in_grade_10
+    self.first_X_students_in_grade_10(1)
+  end
 end
